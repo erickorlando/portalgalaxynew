139c9a075e3827072d9b35a18d8dcb6749b428dc
Erick Orlando <erickorlando@outlook.com>
2024-07-31 22:00:02 -0500

    Cambios en el frontend con bootstrap

--- Portal Galaxy/PortalGalaxy/PortalGalaxy.Server/Program.cs
+++ Portal Galaxy/PortalGalaxy/PortalGalaxy.Server/Program.cs
@@ -15,6 +15,7 @@ builder.Services.Configure<AppSettings>(builder.Configuration);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -91,16 +92,26 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    //app.UseWebAssemblyDebugging();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+//app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
+
 
 using (var scope = app.Services.CreateScope())
 {
